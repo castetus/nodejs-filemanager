e3e855bd561b4ead89717d3207b494cdb7649819
castetus <kboikov@mail.ru>
2024-10-13 09:43:04 +0200

    refactor zip

--- src/zip.js
+++ src/zip.js
@@ -3,12 +3,13 @@ import { createBrotliCompress, createBrotliDecompress } from 'zlib';
 import { pipeline } from 'stream/promises';
 import { successStatus, invalidStatus } from './constants.js';
 
-export const compress = async ([sourcePath, destinationPath]) => {
+const processFile = async (...args) => {
+  const [sourcePath, destinationPath, mode] = args;
   if (!sourcePath || !destinationPath) {
     return { status: invalidStatus };
   }
   const readStream = createReadStream(sourcePath);
-  const processStream = createBrotliCompress();
+  const processStream = mode === 'compress' ? createBrotliCompress() : createBrotliDecompress();
   const writeStream = createWriteStream(destinationPath);
 
   await pipeline(
@@ -19,18 +20,10 @@ export const compress = async ([sourcePath, destinationPath]) => {
   return { status: successStatus };
 };
 
-export const decompress = async ([sourcePath, destinationPath]) => {
-  if (!sourcePath || !destinationPath) {
-    return { status: invalidStatus };
-  }
-  const readStream = createReadStream(sourcePath);
-  const processStream = createBrotliDecompress();
-  const writeStream = createWriteStream(destinationPath);
+export const compress = async ([sourcePath, destinationPath]) => {
+  return processFile(sourcePath, destinationPath, 'compress')
+};
 
-  await pipeline(
-    readStream,
-    processStream,
-    writeStream,
-  );
-  return { status: successStatus };
+export const decompress = async ([sourcePath, destinationPath]) => {
+  return processFile(sourcePath, destinationPath, 'decompress')
 };
